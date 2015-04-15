499cd3ef695a960134746ab8cc6b7fe0a8a3e2f7
sjs20012001 <sjs_20012001@hotmail.com>
2015-04-14 19:37:57 -0700

    speechrecog demo working :)

--- raspiAI/SpeechRecognition.py
+++ raspiAI/SpeechRecognition.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import speech_recognition as sr
+
 __version__ = '0.1.20150412'
 __author__ = "JPSIII and sjs_20012001"
 __url__ = 'https://github.com/joesox/raspiAI'
@@ -12,8 +14,19 @@ class SpeechRecognition(object):
             return 'Attrs()'  
         return '<%s>' % (self.__class__.__name__)
 
+    def HearWAVfiledemo (self):
+        r = sr.Recognizer()
+        with sr.WavFile("ilikepie.wav") as source:          # use "ilikepie.wav" as the audio source
+            audio = r.record(source)                        # extract audio data from the file
+
+        try:
+            print("Transcription: " + r.recognize(audio))   # recognize speech using Google Speech Recognition
+        except LookupError:                                 # speech is unintelligible
+            print("Could not understand audio")
+
 def demo():
     c = SpeechRecognition()
+    c.HearWAVfiledemo()
     print "Finished SpeechRecognition demo!"
 
 if __name__ == '__main__':
