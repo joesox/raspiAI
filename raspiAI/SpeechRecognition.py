5f86418b78c86b31b28ef12b9f0850b8e7a71eaa
joesox <joesox@gmail.com>
2015-04-18 12:28:54 -0700

    checking in

--- raspiAI/SpeechRecognition.py
+++ raspiAI/SpeechRecognition.py
@@ -24,9 +24,19 @@ class SpeechRecognition(object):
         except LookupError:                                 # speech is unintelligible
             print("Could not understand audio")
 
+    def test(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:                # use the default microphone as the audio source
+            audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
+        try:
+            print("You said " + r.recognize(audio))    # recognize speech using Google Speech Recognition
+        except LookupError:                            # speech is unintelligible
+            print("Could not understand audio")
+
 def demo():
     c = SpeechRecognition()
-    c.HearWAVfiledemo()
+    #c.HearWAVfiledemo()
+    c.test()
     print "Finished SpeechRecognition demo!"
 
 if __name__ == '__main__':
